8c738cc4d76041dcff28421dfe9e1baedc09f15b
Naveen William <66348947+naveen61346134@users.noreply.github.com>
2024-10-03 00:03:50 +0530

    Update client.py

--- Chat/client.py
+++ Chat/client.py
@@ -2,7 +2,9 @@ import os
 import sys
 import socket
 import platform
-import threading
+from base64 import b64decode
+from threading import Thread, Event
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
 FORMAT = "UTF-8"
 RX_BUFFER = 1024
@@ -10,25 +12,50 @@ sys.setswitchinterval(0.003)
 cCount = os.cpu_count()
 uname = os.getlogin()
 cs = list(platform.uname())
-sysLog = f"{cs[0]} {cs[2]} {cs[3]} {cs[4]} {cCount} {uname}".encode("UTF-8")
-shutdown = threading.Event()
+sysLog = f"{cs[0]} {cs[2]} {cs[3]} {cs[4]} {cCount} {uname}"
+shutdown = Event()
 clientSoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = input("Enter server ip: ")
 port = int(input("Enter port: "))
 try:
     clientSoc.connect((addr, port))
-    clientSoc.send(sysLog)
 except ConnectionRefusedError:
     print("[-] Host machine refused to connect. Please check the IP!")
     exit(1)
+
+chars = list(ascii_lowercase + ascii_uppercase +
+             digits + punctuation + " " + "\n")
+keyChars = list()
+
+
+def recieveKey():
+    global keyChars
+    keyStr = clientSoc.recv(RX_BUFFER).decode(FORMAT)
+    keyChars = list(b64decode(keyStr).decode(FORMAT))
+    clientSoc.send(encryptMsg(keyChars, sysLog).encode(FORMAT))
+    print(f"[+] Recieved Encryption Key Size : {len(keyChars)}")
+
+
+def encryptMsg(key: list, msg: str):
+    global chars
+    encList = [key[chars.index(char)] for char in msg]
+    encMsg = "".join(encList)
+    return encMsg
+
+
+def decryptMsg(key: list, msg: str):
+    global chars
+    decList = [chars[key.index(char)] for char in msg]
+    decMsg = "".join(decList)
+    return decMsg
 
 
 def sendMsg():
     while not shutdown.is_set():
         try:
-            msg = input(">>> ").encode(FORMAT)
-            clientSoc.send(msg)
-        except KeyboardInterrupt:
+            msg = input(">>> ")
+            clientSoc.send(encryptMsg(keyChars, msg).encode(FORMAT))
+        except (KeyboardInterrupt, EOFError):
             print("[-] closing connection")
             shutdown.set()
 
@@ -36,7 +63,8 @@ def sendMsg():
 def recieveMsg():
     while not shutdown.is_set():
         try:
-            msg = clientSoc.recv(RX_BUFFER).decode(FORMAT)
+            msg = decryptMsg(keyChars, clientSoc.recv(
+                RX_BUFFER).decode(FORMAT))
             if not msg:
                 print("[-] Server closed....")
                 shutdown.set()
@@ -51,13 +79,19 @@ def recieveMsg():
 
 
 def main():
-    user = input("\nEnter username: ").encode(FORMAT)
-    clientSoc.send(user)
+    init = clientSoc.recv(RX_BUFFER).decode(FORMAT)
+    if init == "FULL":
+        print("[-] Server is full.")
+        exit(1)
+    elif init == "KEY":
+        recieveKey()
+    user = input("\nEnter username: ")
+    clientSoc.send(encryptMsg(keyChars, user).encode(FORMAT))
 
-    sendThread = threading.Thread(
+    sendThread = Thread(
         target=sendMsg, daemon=True)
     sendThread.start()
-    recieveThread = threading.Thread(target=recieveMsg, daemon=True)
+    recieveThread = Thread(target=recieveMsg, daemon=True)
     recieveThread.start()
 
     sendThread.join()
