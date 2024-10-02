6df4b818e556d8214a7f94f9f6be364bddfcc747
Naveen William <66348947+naveen61346134@users.noreply.github.com>
2024-10-02 23:22:13 +0530

    msg encryption and optimization server.py

--- Chat/base(Stripped)/server.py
+++ Chat/base(Stripped)/server.py
@@ -1,8 +1,11 @@
+import base64
+import string
+import random
 import socket
 import threading
 
 clients = []
-maxClients = 4
+maxClients = 2
 FORMAT = "UTF-8"
 RX_BUFFER = 1024
 clientLock = threading.Lock()
@@ -22,24 +25,63 @@ except OSError:
 print(f"[+] Local Server up and running!")
 serverSoc.listen(maxClients)
 
+chars = list(string.ascii_lowercase + string.ascii_uppercase +
+             string.digits + string.punctuation + " " + "\n")
+keyChars = chars.copy()
+random.shuffle(keyChars)
+
+
+def sendKey(soc: socket.socket):
+    kstr = "".join(keyChars)
+    baseKey = base64.b64encode(kstr.encode(FORMAT)).decode(FORMAT)
+    soc.send(baseKey.encode(FORMAT))
+
+
+def encryptMsg(key: list, msg: str):
+    global chars
+    encList = [key[chars.index(char)] for char in msg]
+    encMsg = "".join(encList)
+    return encMsg
+
+
+def decryptMsg(key: list, msg: str):
+    global chars
+    decList = [chars[key.index(char)] for char in msg]
+    decMsg = "".join(decList)
+    return decMsg
+
 
 def broadcast(msg: str, user=None):
     with clientLock:
         for client in clients:
             if user != None:
-                client.send(f"[{user}]: {msg}".encode(FORMAT))
+                encMsg = encryptMsg(keyChars, f"[{user}]: {msg}")
+                client.send(encMsg.encode(FORMAT))
             else:
-                client.send(msg.encode(FORMAT))
+                client.send(encryptMsg(keyChars, msg).encode(FORMAT))
 
 
-def handleClient(cSoc: socket.socket, cIP):
-    username = cSoc.recv(RX_BUFFER).decode(FORMAT)
+def handleClient(cSoc: socket.socket):
+    cSoc.send("KEY".encode(FORMAT))
+    sendKey(cSoc)
+    print(f"[+] Send Encryption Key Size: {len(keyChars)}")
+    try:
+        username = decryptMsg(keyChars, cSoc.recv(RX_BUFFER).decode(FORMAT))
+    except ConnectionResetError:
+        print("[-] Client disconnected!\n")
+        clients.remove(cSoc)
+        cSoc.close()
     print(f"[+] {username} joined the server")
     broadcast(f"[+] {username} joined the chat\n")
     while True:
         try:
             msg = cSoc.recv(RX_BUFFER).decode(FORMAT)
-            broadcast(msg, username)
+            decMsg = decryptMsg(keyChars, msg)
+            broadcast(decMsg, username)
+        except ConnectionResetError:
+            print("[-] Client disconnected!\n")
+            clients.remove(cSoc)
+            cSoc.close()
         except:
             with clientLock:
                 clients.remove(cSoc)
@@ -55,15 +97,17 @@ def main():
         try:
             clientSoc, clinetIP = serverSoc.accept()
             if len(clients) >= maxClients:
+                print(
+                    f"\n[*] {clinetIP} trying to connect (MAX CLIENT ERROR)\n")
                 clientSoc.send(
-                    "Server is full, try again later.".encode(FORMAT))
+                    "FULL".encode(FORMAT))
                 clientSoc.close()
                 continue
             print(f"\n[+] Got connection from {clinetIP[0]}!")
             with clientLock:
                 clients.append(clientSoc)
             clientThread = threading.Thread(
-                target=handleClient, args=(clientSoc, clinetIP))
+                target=handleClient, args=(clientSoc,))
             clientThread.start()
         except socket.timeout:
             continue
