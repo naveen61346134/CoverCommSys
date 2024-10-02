61e7576f6ce4c60eb2dda234b01a06691b861522
Naveen William <66348947+naveen61346134@users.noreply.github.com>
2024-10-02 23:22:44 +0530

    Optimized client.py

--- Chat/base(Stripped)/client.py
+++ Chat/base(Stripped)/client.py
@@ -1,3 +1,5 @@
+import base64
+import string
 import socket
 import threading
 
@@ -14,12 +16,38 @@ except ConnectionRefusedError:
     print("[-] Host machine refused to connect. Please check the IP!")
     exit(1)
 
+chars = list(string.ascii_lowercase + string.ascii_uppercase +
+             string.digits + string.punctuation + " " + "\n")
+keyChars = list()
+
+
+def recieveKey():
+    global keyChars
+    keyStr = clientSoc.recv(RX_BUFFER).decode(FORMAT)
+    keyChars = list(base64.b64decode(keyStr).decode(FORMAT))
+    print(f"[+] Recieved Encryption Key Size : {len(keyChars)}")
+
+
+def encryptMsg(key: list, msg: str):
+    global chars
+    encList = [key[chars.index(char)] for char in msg]
+    encMsg = "".join(encList)
+    return encMsg
+
+
+def decryptMsg(key: list, msg: str):
+    global chars
+    decList = [chars[key.index(char)] for char in msg]
+    decMsg = "".join(decList)
+    return decMsg
+
 
 def sendMsg():
     while not shutdown.is_set():
         try:
-            msg = input().encode(FORMAT)
-            clientSoc.send(msg)
+            msg = input()
+            encMsg = encryptMsg(keyChars, msg)
+            clientSoc.send(encMsg.encode(FORMAT))
         except KeyboardInterrupt:
             print("[-] closing connection")
             shutdown.set()
@@ -29,13 +57,21 @@ def recieveMsg():
     while not shutdown.is_set():
         try:
             msg = clientSoc.recv(RX_BUFFER).decode(FORMAT)
+            if msg == "FULL":
+                print("[-] Server is full.")
+                shutdown.set()
+            decMsg = decryptMsg(keyChars, msg)
+
             if not msg:
                 print("[-] Server closed....")
                 shutdown.set()
             else:
-                print(msg)
+                print(decMsg)
         except KeyboardInterrupt:
             print("[-] closing connection")
+            shutdown.set()
+        except ConnectionResetError:
+            print("[-] Server has been shutdown!")
             shutdown.set()
         except Exception as e:
             print(f"[-] Error occured: {e}")
@@ -43,8 +79,14 @@ def recieveMsg():
 
 
 def main():
-    user = input("\nEnter username: ").encode(FORMAT)
-    clientSoc.send(user)
+    init = clientSoc.recv(RX_BUFFER).decode(FORMAT)
+    if init == "FULL":
+        print("[-] Server is full.")
+        exit(1)
+    elif init == "KEY":
+        recieveKey()
+    user = input("\nEnter username: ")
+    clientSoc.send(encryptMsg(keyChars, user).encode(FORMAT))
 
     sendThread = threading.Thread(
         target=sendMsg, daemon=True)
